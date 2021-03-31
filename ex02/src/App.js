b5e096eaf4a450554955fb1ea02e4cf846eff95b
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-04-01 00:11:14 +0200

    Cookie - Local Storage - Session Storage - Get

---
+++ ex02/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  setMyStorage = () => {
+    document.cookie = "Year=2021";
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  };
+
+  getMyStorage = () => {
+    let myCookieData = document.cookie.split("=")[1];
+    let myLocalStorageData = localStorage.getItem("Paragon");
+    let mySessionStorageData = sessionStorage.getItem("frontend");
+
+    //console.log([myCookieData, myLocalStorageData, mySessionStorageData]);
+    return [myCookieData, myLocalStorageData, mySessionStorageData];
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.setMyStorage}>Set Storage</button>
+        <button onClick={this.getMyStorage}>Get Storage</button>
+      </div>
+    );
+  }
+}
+
+export default App;
